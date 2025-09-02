ba5262487daf0a6dbbeb0c141c512b782ccb5b45
Reynier Calulo <calulo_r@ca01.ub.lan>
2025-09-02 11:57:37 +0800

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+// Import your components
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.Component';
+import { ContactComponent } from './contact/contact.Component';
+
+// Define the routes
+export const routes: Routes = [
+  { path: '', component: HomeComponent },       // Home route (default)
+  { path: 'about', component: AboutComponent },  // About route
+  { path: 'contact', component: ContactComponent } // Contact route
+];
